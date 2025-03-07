46f70dd84dfc8cf2e4e2ebf0ba1c9260706f87c9
Maria-Adalgisa <mariaadalgisasouza18@gmail.com>
2025-03-07 19:03:47 -0300

    curso NodeJs (07 março 25)

--- CURSO-NODEJS-DICASPARADEVS/server.js
+++ CURSO-NODEJS-DICASPARADEVS/server.js
@@ -1,9 +1,52 @@
-import { createServer } from "node:http";
+import { fastify } from "fastify";
+import { DatabaseMemory } from "./database-memory.js";
 
-const server = createServer(() => {
-  console.log("O Servidor rodando!");
+const server = fastify();
+
+const database = new DatabaseMemory();
+// POST http://localhost:3332/videos
+// Request Body
+
+server.post("/videos", (request, reply) => {
+  const { title, description, duration } = request.body;
+  //console.log(body);
+  database.create({
+    title,
+    description,
+    duration,
+  });
+
+  return reply.status(201).send();
 });
-server.listen(8081);
 
-//localhost: 3333
-//localhost: 3332
+server.get("/videos", () => {
+  const videos = database.list();
+  console.log(videos);
+
+  return videos;
+});
+// Route parameter
+
+server.put("/videos/:id", (request, reply) => {
+  const videoId = request.params.id;
+  const { title, description, duration } = request.body;
+
+  database.update(videoId, {
+    title,
+    description,
+    duration,
+  });
+
+  return reply.status(204).send();
+});
+
+server.delete("/videos/:id", (request, reply) => {
+  const videoId = request.params.id;
+
+  database.delete(videoId);
+  return reply.status(204).send();
+});
+// GET, POST, PUT, DELETE, PATCH
+server.listen({
+  port: 3332,
+});
---
+++ curso-python/cursoemvideo/num-aleatorio.py
@@ -0,0 +1,8 @@
+# import random
+# num = random.randint(1, 10)
+# print(num)
+import random
+
+for _ in range(6):
+    num = random.randint(1, 50)
+    print(num)
